0ed67fc5d1020a8fc73f1c82b0eb458ce3e1d1b3
Daniel <danrpts@gmail.com>
2017-02-19 01:02:05 -0800

    updated comments

--- rule.js
+++ rule.js
@@ -1,11 +1,12 @@
 
 // public rule fxn
-// n is rule # 1-256 (curry in)
-// c is cell value (self)
-// i is cell index (self)
-// a is automaton (array)
+// n is rule # 1-256 (curried in)
+// c is cell value
+// i is cell index
+// a is whole automaton
 module.exports = function (n, c, i, a) {
 
+  // cells count
   var h = a.length;
 
   // left cell idx
@@ -14,19 +15,19 @@ module.exports = function (n, c, i, a) {
   // right cell idx
   var ri = (i + 1) % h;
 
-  // interpret as decimal
+  // interpret neighborhood pattern as decimal
   var d = 4 * a[li] + 2 * c + 1 * a[ri];
 
-  // convert to binary string
+  // convert decimal rule # to binary string
   var b = n.toString(2);
 
-  // implicit mod 256 for n greater than 256
+  // mod 256 for rule # greater than 256
   if (b.length > 8) b = b.substr(b.length - 8);
 
-  // pad high order bits w/ 0s when neccessary
+  // pad high order bits of binary rule # w/ 0s if neccessary
   while (b.length < 8) b = '0' + b;
 
-  // map pattern onto n
+  // map neighborhood pattern onto binary rule #
   return parseInt(b[7 - d], 2);
 
 }
